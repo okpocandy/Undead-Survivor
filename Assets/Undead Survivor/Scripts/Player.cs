88305b28fde0bc72856bc3f4fcb24d655dadb2a6
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-01 17:25:59 +0900

    Add Stand, Run, Dead animations
    Commit Summary: Add Stand, Run, Dead animations
    
    Commit Message: Implemented Stand, Run, and Dead animations for characters/entities within the game.

--- Assets/Undead Survivor/Scripts/Player.cs
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -10,9 +10,13 @@ public class Player : MonoBehaviour
     [SerializeField] float speed = 2;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void OnMove(InputValue value)
@@ -24,5 +28,15 @@ public class Player : MonoBehaviour
     {
         Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x !=0 )
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
