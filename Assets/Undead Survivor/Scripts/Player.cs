44c7d5e435c901472707ffd8be9ccde65ed1bcd0
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-01 16:34:54 +0900

    Add assets and implement movement using InputSystem
    Added necessary assets to the game and implemented player movement using Unity's InputSystem.

---
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Vector2 inputVec;
+    [SerializeField] float speed = 2;
+
+    Rigidbody2D rigid;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+}
