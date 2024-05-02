1b2b90bf8ba7bdbe4649957695df62c1b3626eda
okpocandy <71319122+okpocandy@users.noreply.github.com>
2024-05-02 18:59:21 +0900

    Add enemy that tracks the player and respawns when out of range
    Implemented an enemy that tracks the player within a certain range and respawns when the player moves out of that range.

--- Assets/Undead Survivor/Scripts/Reposition.cs
+++ Assets/Undead Survivor/Scripts/Reposition.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+
+    private void Awake()
+    {
+        coll = GetComponent<Collider2D>();  
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
@@ -11,8 +18,8 @@ public class Reposition : MonoBehaviour
 
         Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 myPos = transform.position;
-        float diffx = Mathf.Abs(playerPos.x - myPos.x);
-        float diffy = Mathf.Abs(playerPos.y - myPos.y);
+        float diffX = Mathf.Abs(playerPos.x - myPos.x);
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
 
         Vector3 playerDir = GameManager.instance.player.inputVec;
         float dirX = playerDir.x < 0 ? -1 : 1;
@@ -21,13 +28,21 @@ public class Reposition : MonoBehaviour
         switch(transform.tag)
         {
             case "Ground":
-                if (diffx > diffy)
+                if (Mathf.Abs( diffX - diffY) <= 0.1f)
+                {
                     transform.Translate(Vector3.right * dirX * 40);
-                else if (diffx < diffy)
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                else if (diffX > diffY)
+                    transform.Translate(Vector3.right * dirX * 40);
+                else if (diffX < diffY)
                     transform.Translate(Vector3.up * dirY * 40);
                 break;
             case "Enemy":
-
+                if (coll.enabled)
+                {
+                    transform.Translate(playerDir * 25 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0));
+                }
                 break;
         }
     }
